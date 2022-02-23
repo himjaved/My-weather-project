0f0eed4525c5e98f0328434fbad77faa858c68a7
HImja Ved <himja1414@gmail.com>
2022-02-23 13:33:29 +1100

    created css and js files
    Updated project html, css and js files with weather search engine coding.

---
+++ script.js
@@ -0,0 +1,74 @@
+let date = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let displayDay = days[date.getDay()];
+let displayDate = date.getDate();
+let displayHour = date.getHours();
+if (displayHour < 10) {
+  displayHour = `0${displayHour}`;
+}
+
+let displayMinute = date.getMinutes();
+if (displayMinute < 10) {
+  displayMinute = `0${displayMinute}`;
+}
+
+let showDate = document.querySelector("#showDate");
+showDate.innerHTML = `${displayDay} ${displayHour}:${displayMinute}`;
+function showTemperature(response) {
+  console.log(response.data.name);
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = response.data.name;
+  let displayTemp = Math.round(response.data.main.temp);
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity : ${response.data.main.humidity}%`;
+  let windSpeed = document.querySelector("#wind");
+  windSpeed.innerHTML = `Wind Speed : ${Math.round(
+    response.data.wind.speed
+  )}  km/h`;
+  let description = document.querySelector("#weather-description");
+  description.innerHTML = response.data.weather[0].main;
+  let temperature = document.querySelector("#display-temp");
+  temperature.innerHTML = `${displayTemp} °C`;
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let cityName = document.querySelector("#city-name");
+  let city = cityInput.value;
+  cityName.innerHTML = city[0].toUpperCase() + city.substring(1);
+
+  let apiKey = "1bc306ef820d7e96f756aa75ef67ef95";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let searchCity = document.querySelector("#search-city");
+searchCity.addEventListener("submit", showCity);
+
+function showPosition(position) {
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "1bc306ef820d7e96f756aa75ef67ef95";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function showCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentButton = document.querySelector("#current-location");
+currentButton.addEventListener("click", showCurrentPosition);
