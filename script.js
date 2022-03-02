58d629c2040f91be571b961f6e4294a30e7ecfd1
HImja Ved <himja1414@gmail.com>
2022-03-02 13:06:37 +1100

    removed imperial unit
    added new icons

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ if (displayMinute < 10) {
 }
 
 let showDate = document.querySelector("#showDate");
-showDate.innerHTML = `${displayDay} ${displayHour}:${displayMinute}`;
+showDate.innerHTML = `Last updated : ${displayDay} ${displayHour}:${displayMinute}`;
 function showTemperature(response) {
   let cityName = document.querySelector("#city-name");
   cityName.innerHTML = response.data.name;
@@ -39,14 +39,65 @@ function showTemperature(response) {
   let temperature = document.querySelector("#display-temp");
   temperature.innerHTML = `${displayTemp}`;
   celsiusTemperature = response.data.main.temp;
-
+  console.log(response.data.weather[0].icon);
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `src/images/${response.data.weather[0].icon}.svg`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
+function getForecast(coordinates) {
+  let apiKey = "1bc306ef820d7e96f756aa75ef67ef95";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+  console.log(coordinates);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  console.log(date.getDay());
+  return day;
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  console.log(forecast);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(day.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            day.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            day.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function handleSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
@@ -74,30 +125,9 @@ function showPosition(position) {
 }
 function showCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
+  let cityInput = document.querySelector("#city-input");
+  cityInput.value = "";
 }
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  console.log(celsiusTemperature);
-  let temperature = document.querySelector("#display-temp");
-  temperature.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
-  celsiusTemperatureLink.classList.remove("active");
-  fahrenheitTemperatureLink.classList.add("active");
-}
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperature = document.querySelector("#display-temp");
-  temperature.innerHTML = Math.round(celsiusTemperature);
-  celsiusTemperatureLink.classList.add("active");
-  fahrenheitTemperatureLink.classList.remove("active");
-}
-
-let celsiusTemperatureLink = document.querySelector("#celsius");
-let fahrenheitTemperatureLink = document.querySelector("#fahrenheit");
-fahrenheitTemperatureLink.addEventListener("click", convertToFahrenheit);
-celsiusTemperatureLink.addEventListener("click", convertToCelsius);
-
-let celciusTemperature = null;
 
 let currentButton = document.querySelector("#current-location");
 currentButton.addEventListener("click", showCurrentPosition);
